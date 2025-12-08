d1937321d85042e4130b277476b6296bad3d54df
Yegor Kozhevnikov <ykozhevn@gmail.com>
2025-12-08 17:21:56 -0500

    Implemented iterative infinite diffusion
    Now loops diffusion until the difference between the before and after are negligible according to a threshold variable.

--- TuringPatternMaker.py
+++ TuringPatternMaker.py
@@ -1,31 +1,53 @@
-from PIL import Image
-from PIL import ImageFilter
-from PIL import ImageChops
+from PIL import Image, ImageFilter, ImageChops
+from skimage import io, img_as_float
+import numpy as np
 import sys
-    
-LEVEL = 10
+import math
 
 # open file and convert it to greyscale
 im = Image.open(sys.argv[1]).convert('L')
+previous = im
+diff = math.inf
+STOP_THRESHOLD = 0.01
 
 # from photoshop tutorial: https://www.youtube.com/watch?v=eKLki6-gynU
 # high pass 6px
 # threshold 128
 # gaussion blur 5px
 
-for i in range(int(sys.argv[2])):
-    print(f'Iteration {i+1} of {sys.argv[2]}')
+def highPass(im, px):
+    low_pass = im.filter(ImageFilter.GaussianBlur(px))
+    return ImageChops.subtract(im, low_pass, scale=1.0, offset=128)
+
+def threshold(im, thresh):
+    return im.point(lambda p: p > thresh and 255)
+    
+def getDiff(im1, im2):
+    return abs(np.mean(np.array(im1) - np.array(im2)))
+
+# loop diffusion until the difference is negligible
+i = 0
+while True:
+    i = i + 1
+    previous = im
+    print(f'Iteration {i}')
+    
     # high pass 
-    low_pass = im.filter(ImageFilter.GaussianBlur(10))
-    im = ImageChops.subtract(im, low_pass, scale=1.0, offset=128)
+    im = highPass(im, 6)
     # threshold 
-    im = im.point(lambda p: p > 128 and 255)
+    im = threshold(im, 128)
     # gaussian
     im = im.filter(ImageFilter.GaussianBlur(5))
+    
+    # check difference
+    if abs(diff - getDiff(im, previous)) <= STOP_THRESHOLD:
+        break    
+    diff = getDiff(im,previous)
 
 # make it crisp for viewing
-im = im.point(lambda p: p > 128 and 255)
+im = threshold(im, 128)
+
 im.show()
-#TODO turn sequence of pics into gif w/ ffmpeg
+#TODO turn sequence of pics into gif
 print('Saving...')
 im.save(sys.argv[1].replace(sys.argv[1].split('/')[-1],'out.png'))
